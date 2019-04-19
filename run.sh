807ffeef088cf6b0c2461d1b224d65edfcb55170
Alex Gravenor <blazingkin@gmail.com>
2019-04-19 16:41:59 -0700

    Put all the pieces together

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ export CLASSPATH="$CLASSPATH:./parser"
 java MiniCompiler $1 > compiled.json
 if [ $? -eq 0 ];
 then
-    blz Source/minilang_compiler.blz compiled.json
+    blz Source/minilang_compiler.blz compiled.json ${@:2}
 fi
 rm compiled.json
