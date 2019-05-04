2db1a44dac760291bf2416d6cd94cbb3d0365af2
Alex Gravenor <blazingkin@gmail.com>
2019-05-03 17:07:15 -0700

    Start on dealing with scratch registers

--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 export CLASSPATH="$CLASSPATH:./parser"
-OUTPUT=$(echo $1 | sed 's/\(.*\.\)mini/\1ll/')
+
+if [[ $* == *-llvm* ]];
+then
+    OUTPUT=$(echo $1 | sed 's/\(.*\.\)mini/\1ll/')
+else
+    OUTPUT=$(echo $1 | sed 's/\(.*\.\)mini/\1s/')
+fi
 BINOUTPUT=$(echo $1 | sed 's/\(.*\.\)mini/\1/')
+
+
+
 java -Xss1024m MiniCompiler $1 > compiled.json
 if [ $? -eq 0 ];
 then
