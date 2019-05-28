83344b9c91856227391f2094797ac510940b2b87
Alex Gravenor <blazingkin@gmail.com>
2019-05-28 16:12:53 -0700

    Include blz dependency, state warning about cross compiler dependency

--- Makefile
+++ Makefile
@@ -3,4 +3,5 @@
 clean:
 	-rm brett.* OptimizationBenchmark.* BenchMarkishTopics.* bert.* biggest.* \
 	   binaryConverter.* creativeBenchMarkName.* fact_sum.* Fibonacci.* GeneralFunctAndOptimize.* \
-	   hailstone.* killerBubbles.* mixed.* primes.* programBreaker.* stats.* TicTac.* wasteOfCycles.* 
+	   hailstone.* killerBubbles.* mixed.* primes.* programBreaker.* stats.* TicTac.* wasteOfCycles.* \
+		hanoi_benchmark.* mile1.*
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export CLASSPATH="$CLASSPATH:./parser"
 
+
 if [[ $* == *-llvm* ]];
 then
     OUTPUT=$(echo $1 | sed 's/\(.*\.\)mini/\1ll/')
@@ -8,6 +9,14 @@ then
 else
     OUTPUT=$(echo $1 | sed 's/\(.*\.\)mini/\1s/')
     CC="arm-linux-gnueabi-gcc"
+    if [ -z $(which arm-linux-gnueabi-gcc) ];
+    then
+        echo "Missing arm-linux-gnueabi-gcc dependency"
+        echo "You can install this via sudo apt-get install gcc-arm-linux-gnueabi on a debian system"
+        echo "or sudo pacman -S arm-linux-gnueabi-gcc on arch linux"
+        echo "or sudo yum install gcc-arm-linux-gnu on redhat"
+        exit 1
+    fi
 fi
 BINOUTPUT=$(echo $1 | sed 's/\(.*\.\)mini/\1/')
 
@@ -21,7 +30,7 @@ fi
 java -Xss1024m MiniCompiler $1 > compiled.json
 if [ $? -eq 0 ];
 then
-    blz Source/minilang_compiler.blz compiled.json ${@:2} > $OUTPUT
+    bin/bin/blz Source/minilang_compiler.blz compiled.json ${@:2} > $OUTPUT
     if [ $? -eq 0 -a $COMPILE -eq 1 ];
     then
         $CC $OUTPUT util.c -o $BINOUTPUT
