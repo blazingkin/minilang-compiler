db680fc265d20b142d4823dff4807b599cacdce5
Alex Gravenor <blazingkin@gmail.com>
2019-06-05 16:03:52 -0700

    Fix lots of bugs

--- bmark_test.py
+++ bmark_test.py
@@ -5,5 +5,5 @@ import subprocess
 for bench in os.listdir("benchmarks"):
     subprocess.call(["cp", "benchmarks/{0}/{0}.mini".format(bench), "."])
     print("Compiling {}.mini".format(bench))
-    os.system("./run.sh {0}.mini -compile".format(bench))
+    os.system("./run.sh {0}.mini -llvm -dead-code -cp -compile".format(bench))
     subprocess.call(["rm", "{0}.mini".format(bench)])
