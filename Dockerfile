1b5bf491f224873abcce084780d32eaccd9a6375
Alex Gravenor <blazingkin@gmail.com>
2019-06-10 10:57:51 -0700

    Fix bug in resolving phis

--- Dockerfile
+++ Dockerfile
@@ -9,8 +9,6 @@ RUN apt-get install -y gcc-arm-linux-gnueabi
 
 RUN apt-get install -y default-jre wget
 
-RUN wget https://www.antlr.org/download/antlr-4.7.2-complete.jar > /usr/local/lib/antlr-4.7.2-complete.jar
-
 ENV INDOCKER=true
 
 WORKDIR /app
--- bmark_test.py
+++ bmark_test.py
@@ -1,9 +1,35 @@
 #!/usr/bin/env python3
-import os
+import os, sys
 import subprocess
+from subprocess import Popen, PIPE
 
-for bench in os.listdir("benchmarks"):
-    subprocess.call(["cp", "benchmarks/{0}/{0}.mini".format(bench), "."])
-    print("Compiling {}.mini".format(bench))
-    os.system("./run.sh {0}.mini -llvm -dead-code -cp -dc -sc -compile".format(bench))
-    subprocess.call(["rm", "{0}.mini".format(bench)])
+if len(sys.argv) > 1:
+    to_do = sys.argv[1]
+else:
+    to_do = "compile_all_opts"
+
+flags = sys.argv[2:]
+
+if __name__ == "__main__":
+    if to_do == "compile_all_opts":
+        for bench in os.listdir("benchmarks"):
+            subprocess.call(["cp", "benchmarks/{0}/{0}.mini".format(bench), "."])
+            print("Compiling {}.mini".format(bench))
+            os.system("./run.sh {0}.mini {} -compile".format(bench, " ".join(flags)))
+            subprocess.call(["rm", "{0}.mini".format(bench)])
+    elif to_do == "test_all":
+        for bench in os.listdir("benchmarks"):
+            subprocess.call(["cp", "benchmarks/{0}/{0}.mini".format(bench), "."])
+            print("Compiling {}.mini".format(bench))
+            os.system("./run.sh {0}.mini -llvm {1} -compile".format(bench, " ".join(flags)))
+            subprocess.call(["rm", "{0}.mini".format(bench)])
+            test_input = open("benchmarks/{}/input.longer".format(bench)).read()
+            output = Popen(['./{0}.'.format(bench)], stdin=PIPE, stdout=PIPE).communicate(bytearray(test_input, "utf-8"))[0]
+            output = str(output, "utf-8")
+            test_output = open("benchmarks/{}/output.longer".format(bench)).read()
+            if output != test_output:
+                print("Benchmark {} failed".format(bench))
+                print(output)
+                print(test_output)
+            else:
+                print("Benchmark {} passed".format(bench))
