6f8bea6f3af255ac5394f6439896fe8382e9399d
Alex Gravenor <blazingkin@gmail.com>
2019-06-10 11:34:45 -0700

    Update test script

--- bmark_test.py
+++ bmark_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-import os, sys
+import os, sys, time, statistics
+import matplotlib as plt
 import subprocess
 from subprocess import Popen, PIPE
 
@@ -33,3 +34,27 @@ if __name__ == "__main__":
                 print(test_output)
             else:
                 print("Benchmark {} passed".format(bench))
+    elif to_do == "bench":
+        for bench in os.listdir("benchmarks"):
+            subprocess.call(["cp", "benchmarks/{0}/{0}.mini".format(bench), "."])
+            print("Compiling {}.mini".format(bench))
+            os.system("./run.sh {0}.mini {1} -compile".format(bench, " ".join(flags)))
+            subprocess.call(["rm", "{0}.mini".format(bench)])
+            test_input = open("benchmarks/{}/input.longer".format(bench)).read()
+            deltas = []
+            sum = 0
+            print("Running tests")
+            for i in range(20):
+                starttime = time.time()
+                output = Popen(['./{0}.'.format(bench)], stdin=PIPE, stdout=PIPE).communicate(bytearray(test_input, "utf-8"))[0]
+                endtime = time.time()
+                deltas.append(endtime - starttime)
+                sum += endtime - starttime
+            print(deltas)
+            print("Average {}".format(sum / 20))
+            print("Mean {}".format(statistics.mean(deltas)))
+            print("StdDev {}".format(statistics.stdev(deltas)))
+            r = open("results.csv", "a")
+            r.write("\n{},{},{}".format(bench, " ".join(flags),",".join(map(lambda x: str(x), deltas))))
+            r.close()
+
